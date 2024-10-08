08207edd71797396abe8e451d7785337583cc3bc
micwic <michel.wicky@osima.ch>
2024-10-08 13:37:41 -0400

    adding image to team members

--- loadTeamMembers.js
+++ loadTeamMembers.js
@@ -5,17 +5,27 @@
                     throw new Error('Erreur lors du chargement du fichier team_members.json');
                 }
                 const data = await response.json();
-                const teamContainer = document.getElementById('teamMembers');
+                const teamMembersContainer = document.getElementById('teamMembers');
                 
                 data.teamMembers.forEach(member => {
-                    const memberDiv = document.createElement('div');
-                    memberDiv.classList.add('teamMember');
-                    memberDiv.innerHTML = `
+                    // create member container that will contains the two subsequent container for image and info
+                    const teamMemberContainer = document.createElement('div');
+                    teamMemberContainer.classList.add('teamMemberContainer');
+                    // create image container and image tag into
+                    const teamMemberImage = document.createElement('div');
+                    teamMemberImage.classList.add('teamMemberImage');
+                    teamMemberImage.innerHTML = `<img src="${member.imageURL}" alt="${member.name}" class="memberImage">`;
+                    // 
+                    const teamMemberInfo = document.createElement('div');
+                    teamMemberInfo.classList.add('teamMemberInfo');
+                    teamMemberInfo.innerHTML = `
                         <h2>${member.name}</h2>
                         <p>${member.responsibility}</p>
                         <p class="jobTitle">${member.jobTitle}</p>
                     `;
-                    teamContainer.appendChild(memberDiv);
+                    teamMemberContainer.appendChild(teamMemberImage);
+                    teamMemberContainer.appendChild(teamMemberInfo);
+                    teamMembersContainer.appendChild(teamMemberContainer);
                 });
             } catch (error) {
                 console.error('Erreur:', error);
